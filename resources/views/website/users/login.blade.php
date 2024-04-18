1702cad8e784630bdd4c00c924ae3c26178e871a
Nour Muhammed <nourmuhammed20121994@gmail.com>
2024-04-18 11:03:16 +0200

    for mobile only

--- resources/views/website/users/login.blade.php
+++ resources/views/website/users/login.blade.php
@@ -96,6 +96,16 @@
             width: 150px;
             margin-top: 22px;
         }
+        @media only screen and (max-width: 768px) {
+            /* For mobile phones: */
+            .phone{
+                width:200px
+            }
+            .country{
+                width: 100px;
+                margin-top: 22px;
+            }
+        }
 
     </style>
 
