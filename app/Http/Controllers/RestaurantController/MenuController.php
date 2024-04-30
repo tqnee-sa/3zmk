4b976780543e37d84a858c15c65db95db7a9e5f0
Nour Muhammed <nourmuhammed20121994@gmail.com>
2024-04-30 11:16:54 +0300

    copy menu

--- app/Http/Controllers/RestaurantController/MenuController.php
+++ app/Http/Controllers/RestaurantController/MenuController.php
@@ -265,6 +265,7 @@ class MenuController extends Controller
                                     })->save($destinationPath . '/' . $PImage);
                                 }
                             }
+                            dd($product->video_type);
                             $az_product = AZProduct::create([
                                 'restaurant_id' => $restaurant->id,
                                 'branch_id' => $az_branch->id,
